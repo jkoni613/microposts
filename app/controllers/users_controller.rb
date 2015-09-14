417fcdac7c1d96c66b1b8acbe3a6e2f092e33868
jkoni613 <jkoni613@gmail.com>
2015-09-14 12:39:17 +0000

    add area and profile

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+  def show 
+    @user = User.find(params[:id])
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])  
+    if @user.update(user_params)
+      redirect_to edit_user_path, notice: 'ユーザー情報を更新しました'
+    else
+      # 保存に失敗した場合は編集画面へ戻す
+      render 'edit'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :area, :profile, :password, :password_confirmation)
+  end
+end
