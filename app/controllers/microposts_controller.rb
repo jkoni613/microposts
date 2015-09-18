a0aa00e165a6feb37ff37d6841e32d23b6614b64
jkoni613 <jkoni613@gmail.com>
2015-09-18 08:11:38 +0000

    add followings/followers

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,5 @@
 class MicropostsController < ApplicationController
-  before_action :logged_in_user, only: [:create]
+  before_action :logged_in_user, only: [:create, :destroy]
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -20,6 +20,7 @@ class MicropostsController < ApplicationController
   end
   
   private
+  
   def micropost_params
     params.require(:micropost).permit(:content)
   end
