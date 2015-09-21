c80448a0f0b205c4514d66da250d5826602eee3b
jkoni613 <jkoni613@gmail.com>
2015-09-21 15:03:46 +0000

    microposts-heroku

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
-      p current_user
       @micropost = current_user.microposts.build
-      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc)
+      @feed_items = current_user.feed_items.includes(:user).order(created_at: :desc).page(params[:page]).per(10)
     end
   end
 end
